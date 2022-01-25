b34b2aeb09fdf69a214c51fa7314477598aa68b8
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2022-01-25 12:49:55 +0530

    icons and date

--- src/Project.js
+++ src/Project.js
@@ -1,8 +1,15 @@
 import "./Project.css";
-import { FaGithub } from "react-icons/fa";
-import { MdOutlinePageview } from "react-icons/md";
+import { GrView } from "react-icons/gr";
 
-export function Project({ title, desc, image, giturl, website }) {
+export function Project({
+  title,
+  desc,
+  image,
+  gitfront,
+  gitback,
+  projecttime,
+  website,
+}) {
   const array = ["#react", "#nodejs", "#mongodb", "#html", "#css"];
   return (
     <div className="project">
@@ -15,23 +22,42 @@ export function Project({ title, desc, image, giturl, website }) {
             <h4 className="project-title">{title}</h4>
             <div className="project-link">
               <a
+                href={gitfront}
+                target="_blank"
+                rel="noreferrer"
+                className="link-icon"
+              >
+                <img
+                  className="gitscImg"
+                  src="https://cdn-icons-png.flaticon.com/512/13/13706.png"
+                  alt=""
+                  title="frontend source code"
+                />
+              </a>
+              <a
+                href={gitback}
+                target="_blank"
+                rel="noreferrer"
+                className="link-icon"
+              >
+                <img
+                  className="gitbscImg"
+                  src="https://cdn-icons-png.flaticon.com/512/263/263100.png"
+                  alt=""
+                  title="backend source code"
+                />
+              </a>
+              <a
                 href={website}
                 target="_blank"
                 rel="noreferrer"
                 className="link-icon"
               >
-                <MdOutlinePageview size="32px" color="darkslategray" />
-              </a>
-              <a
-                href={giturl}
-                target="_blank"
-                rel="noreferrer"
-                className="link-icon"
-              >
-                <FaGithub size="30px" color="darkslategray" />
+                <GrView size="26px" title="view app" />
               </a>
             </div>
           </div>
+          <p className="projecttime">{projecttime}</p>
           <p className="project-desc">{desc}</p>
           <div className="tags">
             {array.map((item) => (
--- src/Projects.js
+++ src/Projects.js
@@ -9,18 +9,20 @@ export function Projects() {
       image:
         "https://elements-cover-images-0.imgix.net/37a16df5-688a-4d87-be17-77674edea0cd?auto=compress%2Cformat&fit=max&w=710&s=95b00303372f5c552fcbe365f9bd042c",
       desc: "Social Media MERN stack Application created using React JS, Node JS, MongoDB (Database) & Socket.io for instant, private Chat feature of this application.",
-      giturl:
-        "https://docs.google.com/document/d/1J1E0Gx_JktdO6eTGhyuI-QJ0v2QHP5NyPuMCi4n01Y4/edit?usp=sharing",
+      gitfront: "https://github.com/gokulakrishnaa/social-app",
+      gitback: "https://github.com/gokulakrishnaa/nsocial-app",
+      projecttime: "Nov 2021 - Dec 2021",
       website: "https://cranky-sammet-877c33.netlify.app",
     },
     {
       id: 2,
-      title: "Amazon Clone",
+      title: "E-Commerce",
       image:
         "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2021/08/Amazon-Side-Hustle-Bundle.jpeg",
-      desc: "Amazon Clone is a Full stack Application with full E-Commerce functionality & Payment gateway. This Application is the simplified version which clones Amazon's important functionalities. Users can cand checkout with payment option created using Stripe payment gateway.",
-      giturl:
-        "https://docs.google.com/document/d/1A7r6CXMxvxrwMxiB4aIBNdm2tidd5Bm2FmFo9zmN_m4/edit?usp=sharing",
+      desc: "E-Commerce (Amazon Clone) is a Full stack Application with full E-Commerce functionalities & Payment gateway. Users can checkout with payment option created using Stripe payment gateway.",
+      gitfront: "https://github.com/gokulakrishnaa/amazon-clone-project",
+      gitback: "https://github.com/gokulakrishnaa/Money-Manager-Backend",
+      projecttime: "Nov 2021 - Jan 2022",
       website: "https://priceless-einstein-7996ef.netlify.app",
     },
     {
@@ -29,8 +31,9 @@ export function Projects() {
       image:
         "https://media.istockphoto.com/vectors/programming-design-concept-vector-id947663966?b=1&k=20&m=947663966&s=612x612&w=0&h=MrPXhQekXbc4WY-9M3vLghIMEZt6Cis_7HL7GhJzvpA=",
       desc: "Money Manager application is a MERN stack application created in the Hackathon conducted by GUVI. This Application tracks the user income and expenses and maintains a collective record in the MongoDB database.",
-      giturl:
-        "https://docs.google.com/document/d/1UHIANUNVwRhBMdy9LhArLm4hK8RgCm0m_Iny_Ym2E6Q/edit?usp=sharing",
+      gitfront: "https://github.com/gokulakrishnaa/money-app",
+      gitback: "https://github.com/gokulakrishnaa/Money-Manager-Backend",
+      projecttime: "Oct 2021 - Nov 2021",
       website: "https://ecstatic-hamilton-ce64a3.netlify.app",
     },
     {
@@ -39,8 +42,9 @@ export function Projects() {
       image:
         "https://verstiuk.com/wp-content/uploads/2019/12/Enterprise-Risks-Video-ProductonIllustration-Equipment.png",
       desc: "Movies app is a MERN stack CRUD Application with movie adding,removing, liking & disliking features. Simple app where one can view the description about their favourite movies and can also add movie to this Movies-App.",
-      giturl:
-        "https://docs.google.com/document/d/1_CS837ReQFqZzKGx9DQZWNl7XmqanzQ6frYkWbh2_EA/edit?usp=sharing",
+      gitfront: "https://github.com/gokulakrishnaa/my-app",
+      gitback: "https://github.com/gokulakrishnaa/Nodeclass",
+      projecttime: "Oct 2021",
       website: "https://happy-benz-6c1b46.netlify.app",
     },
     {
@@ -49,14 +53,20 @@ export function Projects() {
       image:
         "https://elements-cover-images-0.imgix.net/6e855666-1764-4019-be9e-998505bd3fdf?auto=compress&crop=edges&fit=crop&fm=jpeg&h=630&w=1200&s=fffbff5628fab9da0e2697d748790e92",
       desc: "Auth App is a Full Stack Authentication application created using React JS, Node JS,Nodemailer package & MongoDB (Database). Utilizes React functional components, React hooks and Context API, Material UI framework, APIs created using Node.js.",
-      giturl:
-        "https://docs.google.com/document/d/1YH7Kscj1htFEyiZBc-S_K1GQhX8Fr8h7QG95DQ9CVQ0/edit?usp=sharing",
+      gitfront: "https://github.com/gokulakrishnaa/url-app",
+      gitback: "https://github.com/gokulakrishnaa/Nodeurltask",
+      projecttime: "Dec 2021",
       website: "https://focused-hopper-3aac6a.netlify.app",
     },
   ];
   return (
     <div className="projects">
       <h2 className="projects-header">My Projects</h2>
+      <div className="project-cred">
+        <h4>Test Credentials :</h4>
+        <p>User Email : testtest@gmail.com</p>
+        <p>Password : test12345</p>
+      </div>
       <div className="project-cards">
         {projectDetails.map((item) => (
           <Project
@@ -64,7 +74,9 @@ export function Projects() {
             title={item.title}
             desc={item.desc}
             image={item.image}
-            giturl={item.giturl}
+            gitfront={item.gitfront}
+            gitback={item.gitback}
+            projecttime={item.projecttime}
             website={item.website}
           />
         ))}
